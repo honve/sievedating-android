1ce6142d960de641677241f81110f0b6f27f02bc
Jim Yang <oraclebone@gmail.com>
2015-07-27 21:29:53 +0800

    remove the Eclipse project

--- src/com/mygame/myfellowship/XMLTest.java
+++
@@ -1,7 +0,0 @@
-package com.mygame.myfellowship;
-import android.test.AndroidTestCase;
-public class XMLTest extends AndroidTestCase {
-	 public void testSomething() {
-		System.out.println("我的测试");
-	 }
-}
--- src/com/mygame/myfellowship/bean/MonitoredActivity.java
+++
@@ -1,7 +0,0 @@
-package com.mygame.myfellowship.bean;
-
-import android.app.Activity;
-
-public class MonitoredActivity extends Activity{
-
-}
